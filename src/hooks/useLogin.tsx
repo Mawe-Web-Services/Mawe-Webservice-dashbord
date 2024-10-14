e313d0888722da274a5f5d0176d3b18a52e71d47
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-10-14 19:54:39 -0300

    refactor(Removed any type): :recycle:

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,9 +1,19 @@
 import { IReturn, RemoteService } from "../remote/remote";
 
-const useLogin = async (data: any): Promise<IReturn<any>> => {
+type User = {
+  email: string;
+  password: string;
+}
+
+type LoginReturn = {
+  code: number;
+  text: string;
+}
+
+const useLogin = async (data: User): Promise<IReturn<LoginReturn>> => {
   const remoteService = new RemoteService();
 
-  const response = await remoteService.remote<any>({
+  const response = await remoteService.remote<LoginReturn>({
     method: "post",
     endpoint: `${process.env.REACT_APP_API_URL}/user/login`,
     authorization: "login-success",
