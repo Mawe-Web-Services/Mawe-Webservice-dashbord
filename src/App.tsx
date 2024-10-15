0193761a55076bde723e1af209c38f90bbbc1526
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-10-15 20:31:00 -0300

    refactor(Modified route app): :recycle:

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import type React from 'react'
-import { useTranslation } from 'react-i18next'
-import Header from './components/Header'
-import Home from './screens/Home'
-
-import { LandingPage, Heading, CtaButton } from './AppStyles'
+import type React from "react";
+import { AppRoutes } from "./routes/routes";
 
 const App: React.FC = () => {
-  const { t } = useTranslation()
-
-  const animateButton = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    const button = e.currentTarget
-    button.classList.remove('animate')
-
-    button.offsetWidth
-    button.classList.add('animate')
-  }
-
   return (
     <div className="App">
-      <Header />
-
-      <LandingPage>
-        <Heading>{t('header.title')}</Heading>
-        <CtaButton onClick={animateButton}>
-          {t('buttonHeader.buttonText')}
-        </CtaButton>
-      </LandingPage>
-      <Home />
+      <AppRoutes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './screens/Login'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <ToastContainer autoClose={3000} theme='colored' closeOnClick />
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <ToastContainer autoClose={3000} theme="colored" closeOnClick />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
 
-reportWebVitals()
+reportWebVitals();
