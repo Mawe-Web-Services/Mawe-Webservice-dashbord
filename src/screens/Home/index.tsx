cf1985a221ca5455ed78fd17be5fe8a774ac6b7b
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-10-16 18:51:20 -0300

    refactor(Reformated code): :recycle:

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,9 @@
-import type React from 'react'
-import { useTranslation } from 'react-i18next'
-import newAsasImg from '../../assets/newasas.png'
-import FeaturesSection from '../../components/FeaturesSection'
-import Footer from '../../components/Footer/Footer'
-import LanguageSwitcher from '../../components/Select/LanguageSwitcher'
-import '../../i18n/index'
+import type React from "react";
+import { useTranslation } from "react-i18next";
+import newAsasImg from "../../assets/newasas.png";
+import FeaturesSection from "../../components/FeaturesSection";
+import Footer from "../../components/Footer/Footer";
+import "../../i18n/index";
 
 import {
   Container,
@@ -12,72 +11,94 @@ import {
   TextContent,
   StyledImage,
   Features,
-} from './HomeStyles'
+  LandingPage,
+  Heading,
+  CtaButton,
+} from "./styles";
+import Header from "../../components/Header";
 
 const Home: React.FC = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+
+  const animateButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const button = e.currentTarget;
+    button.classList.remove("animate");
+
+    button.offsetWidth;
+    button.classList.add("animate");
+  };
 
   return (
     <Container>
+      <Header />
+
+      <LandingPage>
+        <Heading>{t("header.title")}</Heading>
+        <CtaButton onClick={animateButton}>
+          {t("buttonHeader.buttonText")}
+        </CtaButton>
+      </LandingPage>
+
       <TextImageContainer>
         <TextContent>
-          <h2>{t('welcome')}</h2>
-          <p>{t('feature.runtime')}</p>
+          <h2>{t("welcome")}</h2>
+          <p>{t("feature.runtime")}</p>
         </TextContent>
         <StyledImage src={newAsasImg} alt="New asas" />
       </TextImageContainer>
 
       <Features>
         <FeaturesSection
-          title={t('feature.operationalExperience')}
+          title={t("feature.operationalExperience")}
           icon="✅"
           text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
         />
         <FeaturesSection
-          title={t('feature.operationalExperience')}
+          title={t("feature.operationalExperience")}
           icon="🛠️"
           text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
         />
         <FeaturesSection
-          title={t('feature.developerExperience')}
+          title={t("feature.developerExperience")}
           icon="💻"
           text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
         />
         <FeaturesSection
-          title={t('feature.operationalExperience')}
+          title={t("feature.operationalExperience")}
           icon="🔒"
           text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
         />
         <FeaturesSection
-          title={t('feature.operationalExperience')}
+          title={t("feature.operationalExperience")}
           icon="📊"
           text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
         />
 
         <FeaturesSection
-          title={t('feature.operationalExperience')}
+          title={t("feature.operationalExperience")}
           icon="⚙️"
           text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
         />
       </Features>
 
       <section>
-        <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>
-          {t('feature.operationalExperience')}
+        <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
+          {t("feature.operationalExperience")}
         </h1>
         <Features>
           <FeaturesSection
-            title={t('DeploySection.firstCard')}
+            title={t("DeploySection.firstCard")}
             icon="🪙"
             text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
           />
           <FeaturesSection
-            title={t('DeploySection.SecondCard')}
+            title={t("DeploySection.SecondCard")}
             icon="👁️‍🗨️"
             text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
           />
           <FeaturesSection
-            title={t('DeploySection.thirdCard')}
+            title={t("DeploySection.thirdCard")}
             icon="💵"
             text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quaerat quidem voluptatem numquam praesentium, quasi hic reprehenderit labore rem, deleniti dolor assumenda, facilis architecto qui nam ipsa at dolores consequatur."
           />
@@ -85,7 +106,7 @@ const Home: React.FC = () => {
         <Footer />
       </section>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
